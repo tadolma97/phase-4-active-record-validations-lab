4be9a0886ccc872e648e3252d13d0abc2204f2c6
Tashi Dolma <tadolma97@gmail.com>
2022-06-28 15:57:12 -0400

    commit tree 379a44587973aa7462a9164f821b8eeb0215d741

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+    def clickbait?
+        unless title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, 'Your title is not clickbait-y')
+        end
+    end
+
 end
